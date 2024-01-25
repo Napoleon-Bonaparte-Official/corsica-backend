2764a76b7d9afe72795f9d529601f4c6b15263dc
advikg <advik.sd@gmail.com>
2024-01-25 15:19:58 -0800

    copied cpt into repo

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from __init__ import app, db, cors  # Definitions initialization
 
 
 # setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
 # database migrations
@@ -26,8 +24,6 @@ from projects.projects import app_projects # Blueprint directory import projects
 db.init_app(app)
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
