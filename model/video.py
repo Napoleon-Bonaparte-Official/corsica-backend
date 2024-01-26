56dc0e4e762c6f78abb24c3e882d0d2083467ba1
will-w-cheng <77639521+will-w-cheng@users.noreply.github.com>
2024-01-26 15:28:30 -0800

    The video initial commit replacing players

--- model/video.py
+++ model/video.py
@@ -11,22 +11,23 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 ''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
 
-class Player(db.Model):
-    __tablename__ = 'players'  # table name is plural, class name is singular
+class Video(db.Model):
+    __tablename__ = 'videos'  # table name is plural, class name is singular
 
-    # Define the Player schema with "vars" from object
+    # Define the video schema with "vars" from object
     id = db.Column(db.Integer, primary_key=True)
     _name = db.Column(db.String(255), unique=False, nullable=False)
-    _uid = db.Column(db.String(255), unique=True, nullable=False)
-    _password = db.Column(db.String(255), unique=False, nullable=False)
-    _tokens = db.Column(db.Integer)    
+    _description = db.Column(db.String(255), unique=True, nullable=False)
+    _views = db.Column(db.Integer)
+    _vid = db.Column(db.String(255), unique=True, nullable=False)
+    _path = db.Column(db.String(255), unique=False, nullable = False)
 
-    # constructor of a Player object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, tokens, password="123qwerty"):
+    # constructor of a video object, initializes the instance variables within object (self)
+    def __init__(self, name, description, views, vid, path):
         self._name = name    # variables with self prefix become part of the object, 
-        self._uid = uid
-        self.set_password(password)
-        self._tokens = tokens
+        self._description = description
+        self.views = views
+        self._vid = vid
 
     # a name getter method, extracts name from object
     @property
@@ -40,42 +41,38 @@ class Player(db.Model):
     
     # a getter method, extracts email from object
     @property
-    def uid(self):
-        return self._uid
+    def description(self):
+        return self._description
     
     # a setter function, allows name to be updated after initial object creation
-    @uid.setter
-    def uid(self, uid):
-        self._uid = uid
+    @description.setter
+    def description(self, description):
+        self.description = description
         
-    # check if uid parameter matches user id in object, return boolean
-    def is_uid(self, uid):
-        return self._uid == uid
+    # check if vid parameter matches user id in object, return boolean
+        # a getter method, extracts email from object
+    @property
+    def vid(self):
+        return self._vid
+    
+    # a setter function, allows name to be updated after initial object creation
+    @vid.setter
+    def vid(self, vid):
+        self._vid = vid
+        
+    # check if vid parameter matches user id in object, return boolean
+    def is_Vid(self, vid):
+        return self._vid == vid
     
     @property
-    def password(self):
-        return self._password[0:10] + "..." # because of security only show 1st characters
-
-    # update password, this is conventional setter
-    def set_password(self, password):
-        """Create a hashed password."""
-        self._password = generate_password_hash(password, "pbkdf2:sha256", salt_length=10)
-        
-    # check password parameter versus stored/encrypted password
-    def is_password(self, password):
-        """Check against hashed password."""
-        result = check_password_hash(self._password, password)
-        return result
+    def path(self):
+        return self._path
     
-    # dob property is returned as string, to avoid unfriendly outcomes
-    @property
-    def tokens(self):
-        return self._tokens
+    @path.setter
+    def path(self, path):
+        self._path = path
     
-    # dob should be have verification for type date
-    @tokens.setter
-    def tokens(self, tokens):
-        self._tokens = tokens
+    
     
     
     # output content using str(object) in human readable form, uses getter
@@ -87,7 +84,7 @@ class Player(db.Model):
     # returns self or None on error
     def create(self):
         try:
-            # creates a player object from Player(db.Model) class, passes initializers
+            # creates a video object from video(db.Model) class, passes initializers
             db.session.add(self)  # add prepares to persist person object to Users table
             db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
             return self
@@ -101,58 +98,57 @@ class Player(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "uid": self.uid,
-            "tokens": self.tokens,
-            "password": self._password
+            "description": self.description,
+            "views": self.views,
+            "vid": self.vid,
+            "path": self.path
         }
 
-    # CRUD update: updates name, uid, password, tokens
+    # CRUD update: updates name, vid, password, tokens
     # returns self
     def update(self, dictionary):
         """only updates values in dictionary with length"""
         for key in dictionary:
             if key == "name":
                 self.name = dictionary[key]
-            if key == "uid":
-                self.uid = dictionary[key]
-            if key == "password":
-                self.set_password(dictionary[key])
-            if key == "tokens":
-                self.tokens = dictionary[key]
+            if key == "description":
+                self.description = dictionary[key]
+            if key == "views":
+                self.views = dictionary[key]
+            if key == "vid":
+                self.vid = dictionary[key]
+            if key == "path":
+                self.path = dictionary[key]
         db.session.commit()
         return self
 
     # CRUD delete: remove self
     # return self
     def delete(self):
-        player = self
+        video = self
         db.session.delete(self)
         db.session.commit()
-        return player
+        return video
 
 
 """Database Creation and Testing """
 
 
 # Builds working data for testing
-def initPlayers():
+def initvideos():
     with app.app_context():
         """Create database and tables"""
         db.create_all()
         """Tester records for table"""
-        players = [
-            Player(name='Azeem Khan', uid='azeemK', tokens=45),
-            Player(name='Ahad Biabani', uid='ahadB', tokens=41),
-            Player(name='Akshat Parikh', uid='akshatP', tokens=40),
-            Player(name='Josh Williams', uid='joshW', tokens=38),
-            Player(name='John Mortensen', uid='johnM', tokens=35)
+        videos = [
+            video(name='Azeem Khan', vid='azeemK', tokens=45),
         ]
 
         """Builds sample user/note(s) data"""
-        for player in players:
+        for video in videos:
             try:
-                player.create()
+                video.create()
             except IntegrityError:
                 '''fails with bad or duplicate data'''
                 db.session.remove()
-                print(f"Records exist, duplicate email, or error: {player.uid}")
+                print(f"Records exist, duplicate email, or error: {video.vid}")
