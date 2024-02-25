8c27163aab70c8a7d3e17563e3effe40b6cc8957
advikg <advik.sd@gmail.com>
2024-02-24 23:02:43 -0800

    added github page to cors

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def videos(path):
 def before_request():
     # Check if the request came from a specific origin
     allowed_origin = request.headers.get('Origin')
-    if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io']:
+    if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io', 'https://napoleon-bonaparte-official.github.io/']:
         cors._origins = allowed_origin
 
 # Create an AppGroup for custom commands
