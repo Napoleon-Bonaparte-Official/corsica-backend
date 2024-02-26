d7e9b33244040f2387269cc105c0485a37bf0dfe
will-w-cheng <77639521+will-w-cheng@users.noreply.github.com>
2024-02-25 21:51:52 -0800

    More Cors debugging

--- main.py
+++ main.py
@@ -1,44 +1,32 @@
 import threading
-
-# import "packages" from flask
-from flask import render_template,request, send_from_directory  # import render_template from "public" flask libraries
+from flask import render_template, request, send_from_directory
 from flask.cli import AppGroup
-
-
-# import "packages" from "this" project
-from __init__ import app, db  # Definitions initialization
-
-# setup APIs
-from api.user import user_api # Blueprint import api definition
+from flask_cors import CORS  # Import CORS from flask_cors
+from __init__ import app, db
+from api.user import user_api
 from api.video import video_api
-# from api.player import player_api
-# database migrations
 from model.users import initUsers, initVideos
-# from model.players import initPlayers
-
-# setup App pages
-from projects.projects import app_projects # Blueprint directory import projects definition
-
+from projects.projects import app_projects
 
 # Initialize the SQLAlchemy object to work with the Flask app instance
 db.init_app(app)
 
-# register URIs
-app.register_blueprint(user_api) # register api routes
+# Register URIs
+app.register_blueprint(user_api)
 app.register_blueprint(video_api)
-# app.register_blueprint(player_api)
-app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(app_projects)
 
-@app.errorhandler(404)  # catch for URL not found
+# Define error handler for 404
+@app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
-@app.route('/')  # connects default URL to index() function
+# Define routes
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/table/')  # connects /stub/ URL to stub() function
+@app.route('/table/')
 def table():
     return render_template("table.html")
 
@@ -54,14 +42,12 @@ custom_cli = AppGroup('custom', help='Custom commands')
 def generate_data():
     initUsers()
     initVideos()
-    # initPlayers()
 
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
         
-# this runs the application on the development server
+# Run the application
 if __name__ == "__main__":
-    # change name for testing
-    from flask_cors import CORS
+    # Initialize CORS with origins and credentials support
     CORS(app, origins=['https://napoleon-bonaparte-official.github.io'], supports_credentials=True)
     app.run(debug=True, host="0.0.0.0", port="8069")
