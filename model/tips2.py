b7d5eec91ca2da259749148995f8a157816469bb
will-w-cheng <77639521+will-w-cheng@users.noreply.github.com>
2024-03-26 10:44:29 -0700

    Tips 2 - look at differences between yeongsu and me

---
+++ model/tips2.py
@@ -0,0 +1,156 @@
+## Python Titanic Model, prepared for a titanic.py file
+
+# Import the required libraries for the TitanicModel class
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+class TipsModel:
+    """This whole class encompasses the total tip model to determine how much a customer will tip based on other facts
+    """
+    # This is the instance which stores the entire model, you can use this model many times in prediction, but it's initialized once
+    _instance = None
+    
+    # constructor, used to initialize the TitanicModel
+    def __init__(self):
+        self.tips_data = sns.load_dataset('tips')
+        # Determine the  model
+        self.model = None
+        self.dt = None
+        # define ML features and target
+        self.features = ['total_bill', 'sex', 'smoker', 'dayThur', 'dayFri', "daySat", "daySun", 'time', 'size']
+        self.target = 'tip'
+
+        self.tips_data = sns.load_dataset('tips')
+
+        # Update self.features to match the updated column names
+        self.encoder = OneHotEncoder(handle_unknown='ignore')
+        # load the titanic dataset
+
+
+
+
+    # clean the tips dataset, prepare it for training
+    def _clean(self):
+        self.tips_data['sex'] = self.tips_data['sex'].apply(lambda x: 1 if x == 'male' else 0)
+        self.tips_data['smoker'] = self.tips_data['smoker'].apply(lambda x: 1 if x == 'Yes' else 0)
+        self.tips_data['time'] = self.tips_data['time'].apply(lambda x: 1 if x == 'Dinner' else 0) 
+
+        # Drop rows with missing 'embarked' values before one-hot encoding
+        self.tips_data.dropna(subset=['day'], inplace=True)
+
+        # Categorical data of Day
+        onehot = self.encoder.fit_transform(self.tips_data[['day']]).toarray()
+        cols = ['day' + str(val) for val in self.encoder.categories_[0]]
+        onehot_df = pd.DataFrame(onehot, columns=cols)
+        self.tips_data = pd.concat([self.tips_data, onehot_df], axis=1)
+        self.tips_data.drop(['day'], axis=1, inplace=True)
+
+
+        self.features.extend(cols)
+
+        self.tips_data.dropna(inplace=True)
+
+
+        
+
+      # train the tips model using linear regression
+    def _train(self):
+        # split the data into features and target
+        x = self.tips_data[self.features]
+        y = self.tips_data[self.target]
+
+        # Use LinearRegression for regression tasks
+        self.model = LinearRegression()
+        self.model.fit(x, y)
+
+    @classmethod
+    def get_instance(cls):
+        """ Gets, and conditionally cleans and builds, the singleton instance of the TitanicModel.
+        The model is used for predicting tip amounts based on customer information.
+
+        Returns:
+            TitanicModel: the singleton _instance of the TitanicModel, which contains data and methods for prediction.
+        """        
+        # check for instance, if it doesn't exist, create it
+        if cls._instance is None:
+            cls._instance = cls()
+            cls._instance._clean()
+            cls._instance._train()
+        # return the instance, to be used for prediction
+        return cls._instance
+
+    def predict_tip(self, customer_info):
+        """ Predict the tip amount for a customer based on their information.
+
+        Args:
+            customer_info (dict): A dictionary representing customer information. The dictionary should contain the following keys:
+                'total_bill': The total bill amount
+                'sex': The customer's sex ('Male' or 'Female')
+                'smoker': Whether the customer is a smoker ('Yes' or 'No')
+                'day': The day of the week ('Thur', 'Fri', 'Sat', 'Sun')
+                'time': The time of day ('Lunch' or 'Dinner')
+                'size': The size of the group
+
+        Returns:
+        float: predicted tip amount
+        """
+        # clean the customer data and prepare it for prediction using dataframes from pandas
+        customer_df = pd.DataFrame(customer_info, index=[0])
+        customer_df['sex'] = customer_df['sex'].apply(lambda x: 1 if x == 'Male' else 0)
+        customer_df['smoker'] = customer_df['smoker'].apply(lambda x: 1 if x == 'Yes' else 0)
+        customer_df['time'] = customer_df['time'].apply(lambda x: 1 if x == 'Dinner' else 0) 
+
+        # One-hot encode 'day' feature
+        onehot = self.encoder.transform(customer_df[['day']]).toarray()
+        cols = ['day' + str(val) for val in self.encoder.categories_[0]]
+        onehot_df = pd.DataFrame(onehot, columns=cols)
+        customer_df = pd.concat([customer_df, onehot_df], axis=1)
+        customer_df.drop(['day'], axis=1, inplace=True)
+        
+        # Ensure feature names match those used during training
+        customer_df = customer_df[self.features]
+
+        # Predict tip using the trained model
+        tip = np.squeeze(self.model.predict(customer_df))
+        
+        return tip
+    
+def testTip():
+    """ Test the Titanic Model for predicting tip amounts.
+    Using the TitanicModel class, we can predict the tip amount for a customer.
+    Print output of this test contains method documentation, customer data, and predicted tip amount.
+    """
+     
+    # setup customer data for prediction
+    print(" Step 1: Define customer data for prediction:")
+    customer_info = {
+        'total_bill': [16.99],
+        'sex': ['Female'],
+        'smoker': ['No'],
+        'day': ['Sun'],
+        'time': ['Dinner'],
+        'size': [2]
+    }
+    print("\t", customer_info)
+    print()
+
+    # get an instance of the cleaned and trained Titanic Model
+    tipModel = TipsModel.get_instance()
+    print(" Step 2:", tipModel.get_instance.__doc__)
+   
+    # print the predicted tip amount
+    print(" Step 3:", tipModel.predict_tip.__doc__)
+    tip_amount = tipModel.predict_tip(customer_info)
+    print('\t Predicted tip amount: ${:.2f}'.format(tip_amount))
+    print()
+    
+if __name__ == "__main__":
+    print(" Begin:", testTip.__doc__)
+    testTip()
